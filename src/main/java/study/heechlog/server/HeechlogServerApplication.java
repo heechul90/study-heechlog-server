d377f10d3ae02639ff7adcfa5dc3b6179563bd7a
hclee <heechul4296@gmail.com>
2022-07-09 18:02:15 +0900

    project first commit

---
+++ src/main/java/study/heechlog/server/HeechlogServerApplication.java
@@ -0,0 +1,13 @@
+package study.heechlog.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeechlogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeechlogServerApplication.class, args);
+    }
+
+}
