81b4a0b2db39ee65d63c06721121d39dc6bb02b1
hclee <heechul4296@gmail.com>
2022-07-10 15:16:24 +0900

    post entity 생성

---
+++ src/main/java/study/heechlog/server/core/post/domain/Post.java
@@ -0,0 +1,33 @@
+package study.heechlog.server.core.post.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@SequenceGenerator(
+        name = "post_seq_generator",
+        sequenceName = "post_seq",
+        initialValue = 1, allocationSize = 50
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_seq_generator")
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    //===생성 메서드===//
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
