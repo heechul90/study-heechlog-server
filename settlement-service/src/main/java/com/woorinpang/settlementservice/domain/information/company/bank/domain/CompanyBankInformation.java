f8e31cb6e0dce46e5928a8215e287509e5af4d5d
hclee <heechul4296@gmail.com>
2023-07-26 20:54:34 +0900

    [feature : #50] - bank enum 삭제

--- settlement-service/src/main/java/com/woorinpang/settlementservice/domain/information/company/bank/domain/CompanyBankInformation.java
+++ settlement-service/src/main/java/com/woorinpang/settlementservice/domain/information/company/bank/domain/CompanyBankInformation.java
@@ -1,7 +1,25 @@
 package com.woorinpang.settlementservice.domain.information.company.bank.domain;
 
+import com.woorinpang.settlementservice.domain.bank.domain.Bank;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
 /**
- * 컴퍼니 은행 정보
+ * 컴퍼니 뱅크 정보
  */
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CompanyBankInformation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_bank_information_id") @Comment("컴퍼니 뱅크 정보 고유번호")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bank_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Bank bank;
 }
