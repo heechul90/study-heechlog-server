7196534256c9152d4d5042d10fcdae5fafccb7b2
heechul4296@gmail.com <heechul@vendys.co.kr>
2023-09-01 22:18:13 +0900

    [setting : #8] - application:settlement-api 설정 추가

---
+++ application/settlement-api/src/main/java/com/woorinpang/settlementservice/application/settlement/api/SettlementApiApplication.java
@@ -0,0 +1,13 @@
+package com.woorinpang.settlementservice.application.settlement.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementApiApplication.class, args);
+	}
+
+}
