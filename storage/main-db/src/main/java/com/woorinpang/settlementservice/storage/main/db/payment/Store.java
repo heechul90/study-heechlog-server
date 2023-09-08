d1a823479a59218b397ef62431b4584e9a46acc9
heechul <heechul4296@gmail.com>
2023-09-08 21:56:59 +0900

    [setting : #8]  - storage:main-db payment record 추가

---
+++ storage/main-db/src/main/java/com/woorinpang/settlementservice/storage/main/db/payment/Store.java
@@ -0,0 +1,23 @@
+package com.woorinpang.settlementservice.storage.main.db.payment;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 스토어
+ */
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store {
+    @Column(columnDefinition = "bigint not null comment '스토어 고유번호'")
+    private Long storeId;
+
+    @Column(columnDefinition = "varchar(120) not null comment '스토어명'")
+    private String storeName;
+}
