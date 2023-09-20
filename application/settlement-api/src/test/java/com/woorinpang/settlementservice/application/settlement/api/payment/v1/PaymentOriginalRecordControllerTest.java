1a2f12a54730624f0d49335378939764663c9a4b
heechul <heechul4296@gmail.com>
2023-09-20 20:27:12 +0900

    [feature : #85]  - settlement-api test code 추가

--- application/settlement-api/src/test/java/com/woorinpang/settlementservice/application/settlement/api/payment/v1/PaymentOriginalRecordControllerTest.java
+++ application/settlement-api/src/test/java/com/woorinpang/settlementservice/application/settlement/api/payment/v1/PaymentOriginalRecordControllerTest.java
@@ -1,4 +1,6 @@
 package com.woorinpang.settlementservice.application.settlement.api.payment.v1;
 
-public class PaymentOriginalRecordControllerTest {
+import com.woorinpang.settlementservice.tests.api.docs.RestDocsTest;
+
+public class PaymentOriginalRecordControllerTest extends RestDocsTest {
 }
